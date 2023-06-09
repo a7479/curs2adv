567927fa6375051e1b6a860f0b1db84a3c6dc5ff
Александр <perchatki79@gmail.com>
2023-06-09 12:17:06 +0300

    Initial commit

---
+++ src/main/java/pro/sky/curs2adv/Curs2advApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.curs2adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs2advApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Curs2advApplication.class, args);
+    }
+
+}
